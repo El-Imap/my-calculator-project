671c062b31ba94f3932d6fa43d5afcc757820e6d
El-ImaP <eyolove111@gmail.com>
2022-10-31 10:24:48 +0100

    Add code to sum numbers

---
+++ files/calculator.py
@@ -0,0 +1,5 @@
+firstnumber =  int(input("please enter a number:"))
+secondnumber =  int(input("please enter a second number:"))
+operator = input("what do you intend to do? enter add,sub,mul or div")
+if operator == "add":
+    print("The sum is :", firstnumber + secondnumber)
